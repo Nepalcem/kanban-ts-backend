953aed5f34df7ad23c84d2fd450099196d9cebf8
Nepalcem <mark.emailtest@gmail.com>
2024-02-07 23:16:13 +0200

    re-build

--- dist/controllers/boardController/deleteBoard.js
+++ dist/controllers/boardController/deleteBoard.js
@@ -5,10 +5,12 @@ const models_1 = require("../../models");
 const utils_1 = require("../../utils");
 const deleteBoard = async (req, res, next) => {
     const { hashedID } = req.params;
-    const board = await models_1.Board.deleteOne({ hashedID });
-    if (board.deletedCount === 0) {
+    const board = await models_1.Board.findOne({ hashedID });
+    if (!board) {
         return next(new utils_1.HttpError(404, "Board with such ID doesn't exist"));
     }
+    await models_1.Task.deleteMany({ owner: board._id });
+    await board.deleteOne();
     res.status(200).json({
         message: "Board Deleted!",
     });

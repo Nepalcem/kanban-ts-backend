078be957dd1eeb974e96f8e0f0dd2cde7e737933
Nepalcem <mark.emailtest@gmail.com>
2024-02-08 01:07:21 +0200

    build update

--- dist/routes/taskRouter.js
+++ dist/routes/taskRouter.js
@@ -8,7 +8,7 @@ const tasksController_1 = require("../controllers/tasksController");
 const middleWares_1 = require("../middleWares");
 const validateTaskFields_1 = require("../middleWares/validateTaskFields");
 const taskRouter = express_1.default.Router();
-taskRouter.post("/create", middleWares_1.validateBody, validateTaskFields_1.validateTaskCreationFields, tasksController_1.ctrl.createTask);
+taskRouter.post("/", middleWares_1.validateBody, validateTaskFields_1.validateTaskCreationFields, tasksController_1.ctrl.createTask);
 taskRouter.patch("/:_id", middleWares_1.validateBody, validateTaskFields_1.validateTaskPatchFields, tasksController_1.ctrl.patchTask);
 taskRouter.delete("/:_id", tasksController_1.ctrl.deleteTask);
 exports.default = taskRouter;

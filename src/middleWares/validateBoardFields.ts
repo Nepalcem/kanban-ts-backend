1eca23f4ff195c4a2e27f7fce28f6a7813e1b271
Nepalcem <mark.emailtest@gmail.com>
2024-02-15 13:19:05 +0200

    joi validation error return update

--- src/middleWares/validateBoardFields.ts
+++ src/middleWares/validateBoardFields.ts
@@ -50,8 +50,7 @@ export const validateBoardPatchFields = async (
 
   const { error, value } = boardPatchSchema.validate(data);
   if (error) {
-    console.log(error.message)
-    return next(new HttpError(400, "Bad Request or invalid ID"));
+    return next(new HttpError(400, error.message));
 
   } else {
     req.body = value;
